5fb6670f0907475b5e610bbd1b635bd1a90e202d
yhwjd <yhwjd99@ajou.ac.kr>
2022-08-11 12:18:00 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember(){
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+        //then
+        assertThat(member.getId()).isEqualTo(findMember.getId());
+        assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
