e59c915f0c4de60f3694f503ccb850ea008741b9
yhwjd <yhwjd99@ajou.ac.kr>
2022-08-12 23:32:34 +0900

    jpa-shop

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+}
